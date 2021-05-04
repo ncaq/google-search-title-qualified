3833c2d5c4742a2f729c06db2bceadfbdf6a284d
ncaq <ncaq@ncaq.net>
2021-05-05 00:13:39 +0900

    added: fetch: cookieを送らなかったりキャッシュをなるべく使うようにしたりする

--- src/background/main.ts
+++ src/background/main.ts
@@ -6,7 +6,16 @@ async function listener(message: unknown): Promise<string | undefined> {
   if (typeof message !== "string") {
     throw new Error(`message is not URL: ${JSON.stringify(message)}`);
   }
-  const response = await fetch(message);
+  const response = await fetch(message, {
+    // 妙なリクエストを送らないように制限を加えます(こちらで書かないと変なこと起きないと思いますが)
+    mode: "no-cors",
+    // 認証情報が不用意に送られないようにします。サイトの誤動作防止の意味が強い。
+    credentials: "omit",
+    // 出来るだけブラウザのキャッシュを使っていきます
+    cache: "force-cache",
+    // リダイレクトを追うことを明示的に指定
+    redirect: "follow",
+  });
   if (!response.ok) {
     throw new Error(`response is not ok ${JSON.stringify(response)}`);
   }
