a48b1cf506db974447f7809c2309f0b087a6fde3
ncaq <ncaq@ncaq.net>
2021-05-05 14:07:12 +0900

    added: .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: `${__dirname}/tsconfig.json`,
+  },
+  extends: [
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:import/errors",
+    "plugin:import/typescript",
+    "plugin:import/warnings",
+    "prettier",
+  ],
+  plugins: ["@mysticatea", "@typescript-eslint", "import"],
+  rules: {
+    "@mysticatea/block-scoped-var": "error",
+    "@mysticatea/no-instanceof-array": "error",
+    "@mysticatea/no-instanceof-wrapper": "error",
+    "@mysticatea/no-literal-call": "error",
+    "@mysticatea/no-this-in-static": "error",
+    "@mysticatea/no-use-ignored-vars": "error",
+    "@mysticatea/no-useless-rest-spread": "error",
+    curly: ["error", "all"],
+    "sort-imports": ["error", { ignoreDeclarationSort: true }],
+  },
+};
