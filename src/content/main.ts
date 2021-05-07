8a697e754e65c328957f5b2bddf59bbbc8529f4c
ncaq <ncaq@ncaq.net>
2021-05-07 21:05:22 +0900

    added: linksForFirstView
    [ネットワーク通信に制限を与える · Issue #11 · ncaq/google-search-title-qualified](https://github.com/ncaq/google-search-title-qualified/issues/11)

--- src/content/main.ts
+++ src/content/main.ts
@@ -82,8 +82,12 @@ async function replaceLinkTitles(links: Element[]): Promise<void[]> {
 }
 
 /** エントリーポイント。 */
-async function main(): Promise<void[]> {
-  return replaceLinkTitles(selectLinkElements());
+async function main(): Promise<void> {
+  const links = selectLinkElements();
+  // スクロールせずに表示されるであろうリンクは優先的に処理します
+  const linksForFirstView = links.splice(0, 10);
+  await replaceLinkTitles(linksForFirstView);
+  await replaceLinkTitles(links);
 }
 
 // 検索されるたびに実行します。
