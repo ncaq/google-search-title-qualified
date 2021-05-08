4979defb868d3424faa4861440a1db402be9caea
ncaq <ncaq@ncaq.net>
2021-05-08 19:01:34 +0900

    added: getHtmlTitle: エラー落ちしやすい部分を別関数に分離する

--- src/background/main.ts
+++ src/background/main.ts
@@ -24,40 +24,34 @@ function detectIsUtf8(response: Response, d: Document): boolean {
 
 const domParser = new DOMParser();
 
-/** バックグラウンドプロセス全体のメッセージパッシングを受け取ります */
-async function listener(message: unknown): Promise<string | undefined> {
-  // メッセージ内容がおかしい場合はエラー
-  if (typeof message !== "string") {
-    throw new Error(`message is not URL: ${JSON.stringify(message)}`);
-  }
-  // PDFは読み込まない
-  if (message.endsWith(".pdf")) {
-    return undefined;
-  }
+/** URLからHTMLを取得解析してタイトルを取得します */
+async function getHtmlTitle(urlString: string): Promise<string | undefined> {
   const abortController = new AbortController();
   // ネットワーク通信は10秒でタイムアウト。
   // やたらと時間がかかるサイトはどうせろくでもないことが多い。
   const timeout = setTimeout(() => abortController.abort(), 10000);
   try {
-    const response = await fetch(message, {
+    const response = await fetch(urlString, {
       // 妙なリクエストを送らないように制限を加えます(こちらで書かないと変なこと起きないと思いますが)
       mode: "no-cors",
       // 認証情報が不用意に送られないようにします。サイトの誤動作防止の意味が強い。
       credentials: "omit",
-      // 出来るだけブラウザのキャッシュを使っていきます
+      // 出来るだけブラウザのキャッシュを使っていきます。
       cache: "force-cache",
-      // リダイレクトを追うことを明示的に指定
+      // リダイレクトを追うことを明示的に指定。
       redirect: "follow",
-      // タイムアウト中断コントローラ
+      // タイムアウト中断コントローラ。
       signal: abortController.signal,
     });
     if (!response.ok) {
-      throw new Error(`response is not ok ${JSON.stringify(response)}`);
+      throw new Error(
+        `${urlString}: response is not ok ${JSON.stringify(response)}`
+      );
     }
-    // htmlを直接要求できないのでtextで取得してDOMParserに送り込みます
+    // htmlを直接要求できないのでtextで取得してDOMParserに送り込みます。
     const text = await response.text();
     const dom = domParser.parseFromString(text, "text/html");
-    // UTF-8でない場合取得を諦める
+    // UTF-8でない場合取得を諦めます。
     if (detectIsUtf8(response, dom)) {
       return dom.querySelector("title")?.textContent || undefined;
     }
@@ -69,6 +63,19 @@ async function listener(message: unknown): Promise<string | undefined> {
   } finally {
     clearTimeout(timeout);
   }
+}
+
+/** バックグラウンドプロセス全体のメッセージパッシングを受け取ります */
+async function listener(message: unknown): Promise<string | undefined> {
+  // メッセージ内容がおかしい場合はエラー
+  if (typeof message !== "string") {
+    throw new Error(`message is not URL: ${JSON.stringify(message)}`);
+  }
+  // PDFは読み込まない
+  if (message.endsWith(".pdf")) {
+    return undefined;
+  }
+  return getHtmlTitle(message);
 }
 
 browser.runtime.onMessage.addListener(listener);
