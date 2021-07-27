c4331f03a301439ec7fd04277a5638336f38f690
ncaq <ncaq@ncaq.net>
2021-07-27 20:13:53 +0900

    added: 複数の違うエンコーディングが記述されているサイトにおいてタイトルを取得しない
    例えば、
    [無理無理無理 ドラマチックアラスカ 歌詞情報 - うたまっぷ 歌詞無料検索](http://www.utamap.com/showkasi.php?surl=k-150304-097)
    はHTMLではEUC-JPと言いながら、
    HTML4ではShift_JISを指定して、
    実際にはShift_JISとなっています。
    
    このサイトだけではなく、
    文字エンコードに矛盾が発生しているサイトは多いです。
    そのようなサイトでの文字化けを避けるために、
    矛盾が発生していたら取得は諦めることにしました。

--- src/background/main.ts
+++ src/background/main.ts
@@ -90,15 +90,8 @@ const encodingsRegex: Map<Encoding, RegExp> = new Map([
   ["EUCJP", /EUC[-_]JP/i],
 ]);
 
-/** HTTPとHTMLの情報から文字コードの推定を行います。 */
-function detectEncoding(response: Response, d: Document): Encoding | undefined {
-  const httpContentType = response.headers.get("content-type") || "";
-  const html5Charset =
-    d.querySelector("meta[charset]")?.getAttribute("charset") || "";
-  const html4ContentType =
-    d
-      .querySelector('meta[http-equiv="Content-Type"]')
-      ?.getAttribute("content") || "";
+/** エンコーディング判定用のの正規表現に一致するか判断して、最初に一致したものを返します。 */
+function testEncoding(source: string): Encoding | undefined {
   // forのcontinueなどの機能を使った方がスッキリと書けるので、あえてforを使います。
   // eslint-disable-next-line no-restricted-syntax
   for (const encoding of encodings) {
@@ -107,17 +100,42 @@ function detectEncoding(response: Response, d: Document): Encoding | undefined {
       // eslint-disable-next-line no-continue
       continue;
     }
-    if (
-      re.test(httpContentType) ||
-      re.test(html5Charset) ||
-      re.test(html4ContentType)
-    ) {
+    if (re.test(source)) {
       return encoding;
     }
   }
-  // charsetなどが存在しない場合、
+  return undefined;
+}
+
+/**
+ * HTTPとHTMLの情報から文字コードの推定を行います。
+ * 複数のエンコーディングが指定されていて、
+ * それぞれが矛盾している場合バグだと判断してundefinedを返します。
+ */
+function detectEncoding(response: Response, d: Document): Encoding | undefined {
+  // 判定用の文字列を取得します。
+  const httpContentType = response.headers.get("content-type") || "";
+  const html5Charset =
+    d.querySelector("meta[charset]")?.getAttribute("charset") || "";
+  const html4ContentType =
+    d
+      .querySelector('meta[http-equiv="Content-Type"]')
+      ?.getAttribute("content") || "";
+  // それぞれのソースから計算したエンコーディングを取得します。
+  // 判定不能だったものは除外します。
+  const testedEncodings = [httpContentType, html5Charset, html4ContentType]
+    .map((s) => testEncoding(s))
+    .filter((e): e is NonNullable<typeof e> => e != null);
+  // Setを使って重複を除外します。
+  const encodingsSet = new Set(testedEncodings);
+  // 要素数が1の時のみ正しい結果だと判別します。
+  // 要素数が0の時 == charsetなどが存在しない場合、
   // HTML最新規格ではUTF-8になりますが、
   // そもそも最新規格を参照していないサイトも多いと思うので不明としておきます。
+  if (encodingsSet.size === 1) {
+    // encodingsSetのサイズが1以上であれば、元となった配列にも要素が必ずあるはずです。
+    return testedEncodings[0];
+  }
   return undefined;
 }
 
