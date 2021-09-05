2234fed6e891619292be46dcffa82976f5566efd
ncaq <ncaq@ncaq.net>
2021-09-05 11:00:28 +0900

    added: URLが長い場合書き換えない

--- src/content/url.ts
+++ src/content/url.ts
@@ -2,12 +2,17 @@
  * URLデータをもらい、配下のURL表示を書き換えます。
  */
 function replace(url: string, link: Element): void {
+  // パーセントエンコーディングを解決
+  const decoded = decodeURI(url);
+  // URLが結構長い場合改行が発生してレイアウトがメチャクチャになる可能性が高いため書き換えません。
+  if (decoded.length >= 70) {
+    return;
+  }
   // aの直下ではない部分のURLテキストを書き換えないと中途半端な書き換えになってしまうので、親の要素以下のciteを全書き換え。
   const div = link.parentElement;
   if (div == null) {
     throw new Error("div is null");
   }
-  const decoded = decodeURI(url);
   Array.from(div.querySelectorAll(".TbwUpd cite")).forEach((cite) => {
     // eslint-disable-next-line no-param-reassign
     cite.textContent = decoded;
