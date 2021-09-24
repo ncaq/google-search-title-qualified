92828ca55aca49e8daba2d87d68f99a5df57c190
ncaq <ncaq@ncaq.net>
2021-09-24 20:19:34 +0900

    added: autopagerに対応 closed #12
    [wantora/weautopagerize: Automatically inserts the next page.](https://github.com/wantora/weautopagerize)
    で動作確認しました。

--- src/content/main.ts
+++ src/content/main.ts
@@ -5,21 +5,30 @@ import { replaceLinkUrls } from "./url";
  * 置き換える対象の検索結果要素一覧を取得します。
  * Googleの仕様変更に一番左右されそうな部分。
  */
-function selectLinkElements(): Element[] {
+function selectLinkElements(el: Element): Element[] {
   return Array.from(
-    document.querySelectorAll('.tF2Cxc .yuRUbf a[href^="http"]:not(.fl)')
+    el.querySelectorAll('.tF2Cxc .yuRUbf a[href^="http"]:not(.fl)')
   );
 }
 
 /** エントリーポイント。 */
-async function main(): Promise<void[]> {
-  const links = selectLinkElements();
+async function main(el: Element): Promise<void[]> {
+  const links = selectLinkElements(el);
   const titleP = replaceLinkTitles(links);
   replaceLinkUrls(links);
   return titleP;
 }
 
 // エントリーポイントを実行します。
-main().catch((e) => {
+main(document.documentElement).catch((e) => {
   throw e;
+});
+
+// weAutoPagerizeに対応します。
+document.addEventListener("AutoPagerize_DOMNodeInserted", (event) => {
+  if (event.target instanceof Element) {
+    main(event.target).catch((e) => {
+      throw e;
+    });
+  }
 });
