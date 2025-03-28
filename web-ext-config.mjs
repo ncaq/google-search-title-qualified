e6dea327095022d18dd8257d4c229c5a556032e9
ncaq <ncaq@ncaq.net>
2025-03-28 19:58:37 +0900

    refactor: インポート文の順序を整理
    ESLintによる自動修正。

--- web-ext-config.mjs
+++ web-ext-config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 
 import * as fs from "node:fs";
-import { fileURLToPath } from "node:url";
 import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
