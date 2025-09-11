901a532450212793421d736e46a444d30cff643e
ncaq <ncaq@ncaq.net>
2025-09-11 19:59:50 +0900

    fix: log warnings and return undefined instead of throwing errors
    エラー扱いにしなくてもよい部分で頻繁に例外を投げてしまっていのを、
    警告に収める。

--- src/background/get-html-title.ts
+++ src/background/get-html-title.ts
@@ -6,12 +6,19 @@ export async function getHtmlTitle(url: string): Promise<string | undefined> {
   try {
     const response = await fetchPage(url);
     if (!response.ok) {
-      throw new Error(
+      // eslint-disable-next-line no-console
+      console.warn(
         `${url}: response is not ok. Status: ${response.status.toString()}, StatusText: ${JSON.stringify(response.statusText)}`,
       );
+      return undefined;
     }
     const title = await extractTitle(response);
-    return title && cleanTitle(title);
+    if (title == null) {
+      // eslint-disable-next-line no-console
+      console.warn(`extractTitle returned undefined. url: ${url}`);
+      return undefined;
+    }
+    return cleanTitle(title);
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error("getHtmlTitle error", err, url);
